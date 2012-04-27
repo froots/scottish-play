25ea3c5c7e1da4da71c651f23cf8309cf9670a25
latentflip <phil@latentflip.com>
2012-04-27 15:15:15 +0100

    Use has_many :lines, :through => :paragraphs for character => lines association

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,5 @@
 class Character < ActiveRecord::Base
   attr_accessible :char_id, :description, :name, :short_name, :speech_count
   has_many :paragraphs
-
-  def lines
-    (paragraphs.collect &:lines).flatten # slow as
-  end
+  has_many :lines, :through => :paragraphs
 end
